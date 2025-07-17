13b02f8f83ddb965c33157ed5314d0cfd9369785
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2025-07-17 15:28:37 -0400

    implement react-route

--- index.jsx
+++ index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter,Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+    <Link to="/" >Home</Link>
+    <Link to="/about" >About</Link>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+
+  
+}
+function Home() {
+  return (
+    <h1>Hello, React Router!</h1>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+function About() {
+  return (
+    <h1>About page goes here! 🎉</h1>
+  )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<App/>
+);
