edd4fb3cdc9120fe94f831ff462044ee228de486
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2025-07-19 11:47:51 -0400

    add custom css using NavLink

--- pages/Header.jsx
+++ pages/Header.jsx
@@ -1,13 +1,13 @@
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <NavLink className="site-logo" to="/">#VanLife</NavLink>
         <nav>
-          <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
+          <NavLink to="/about"className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
+          <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
          
         </nav>
       </header>
